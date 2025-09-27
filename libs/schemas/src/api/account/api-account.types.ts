edc8f9373e7e87f7c5fa86924ffb6b200445f9ec
Heiler Nova <heilernova@outlook.com>
2025-09-27 12:48:40 -0500

    feat(schema): agregar interfaces de api-account

---
+++ libs/schemas/src/api/account/api-account.types.ts
@@ -0,0 +1,38 @@
+import { UserGender } from '../../types';
+import { ApiResponseBase } from '../api-response';
+
+export interface ApiAccountInfoResponse extends ApiResponseBase {
+  data: {
+    username: string;
+    email: string;
+    cellphone: string;
+    firstName: string;
+    lastName: string;
+    alias: string | null;
+    sex: UserGender;
+    birthdate: Date;
+    height: number;
+    weight: number;
+    cityId: string;
+    nationality: string;
+    permissions: string[];
+    isCoach: boolean;
+    isJudge: boolean;
+    judgeLevel: string | null;
+    role: string;
+    verified: boolean;
+  }
+}
+
+export interface ApiAccountUpdateBody {
+  username?: string;
+  email?: string;
+  cellphone?: string;
+  firstName?: string;
+  lastName?: string;
+  alias?: string | null;
+  height?: number;
+  weight?: number;
+  cityId?: string;
+  nationality?: string;
+}
