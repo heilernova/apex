dd4a943c871fffdd8ac18772765a9cf2dd28e004
Heiler Nova <heilernova@outlook.com>
2025-11-01 11:37:37 -0500

    feat(web): configurar el service worker y manifest para soportar PWA

--- apps/web/src/app/app.config.ts
+++ apps/web/src/app/app.config.ts
@@ -2,7 +2,7 @@ import {
   ApplicationConfig,
   provideBrowserGlobalErrorListeners,
   provideZonelessChangeDetection,
-  LOCALE_ID,
+  LOCALE_ID, isDevMode,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
@@ -12,6 +12,7 @@ import {
 } from '@angular/platform-browser';
 import { registerLocaleData } from '@angular/common';
 import localeEs from '@angular/common/locales/es-CO';
+import { provideServiceWorker } from '@angular/service-worker';
 
 registerLocaleData(localeEs, 'esCO');
 
@@ -22,5 +23,9 @@ export const appConfig: ApplicationConfig = {
     provideZonelessChangeDetection(),
     provideRouter(appRoutes),
     { provide: LOCALE_ID, useValue: 'esCO' },
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
 };
